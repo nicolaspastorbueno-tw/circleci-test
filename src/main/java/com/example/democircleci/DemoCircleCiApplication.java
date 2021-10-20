8295db787e430ab545e6e3f2d36ea149ef2afeeb
LAnicolas.pastor <lanicolas.pastor@MacBook-Pro-de-LAnicolaspastor.local>
2021-10-20 19:31:21 -0300

    chore: add all

---
+++ src/main/java/com/example/democircleci/DemoCircleCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.democircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCircleCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCircleCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/democircleci/DemoCircleCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCircleCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
